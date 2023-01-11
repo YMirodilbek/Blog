b2139892fe66143d72ec039c8c0d14910f39a718
Mirodilbek <mirodilbek.backend@gmail.com>
2023-01-11 15:39:09 +0500

    changes

--- Blog/settings.py
+++ Blog/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT=BASE_DIR / 'media'
