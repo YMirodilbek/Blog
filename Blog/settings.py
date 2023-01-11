359d9fa640c13ca2ddfaf22f6b46c9c3d7c481c6
Mirodilbek <mirodilbek.backend@gmail.com>
2023-01-11 15:41:48 +0500

    changes

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$r0h4&o-=mk@g4m*ta$l4j!b7mbvssf)tg8uk$%^9^6d=uefy=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
